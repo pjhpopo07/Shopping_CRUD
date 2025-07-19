e957aea67d364bab0aaf27f7420afc1a006e16de
pjhpopo07 <pjhpopo07@gmail.com>
2025-07-19 16:58:45 +0900

    GIT 연동

---
+++ src/test/java/com/example/Shopping_CRUD/ShoppingCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Shopping_CRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
