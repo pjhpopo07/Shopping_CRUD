73f63cfa598b4416d6304f4157925c0a834a79fa
pjhpopo07 <pjhpopo07@gmail.com>
2025-07-19 19:32:24 +0900

    기능 명세 및 컨트롤러 생성

---
+++ src/main/java/com/example/Shopping_CRUD/Controller/MainP.java
@@ -0,0 +1,17 @@
+package com.example.Shopping_CRUD.Controller;
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainP {
+
+    @GetMapping("/main")
+    public String main() {
+        return "shopping";
+    }
+
+}
