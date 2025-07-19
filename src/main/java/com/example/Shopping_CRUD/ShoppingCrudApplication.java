d2b04d005ff4db789f5c71111adcffef89630a39
pjhpopo07 <pjhpopo07@gmail.com>
2025-07-19 17:25:43 +0900

    build 사라져서 다시 커밋 및 푸시

---
+++ src/main/java/com/example/Shopping_CRUD/ShoppingCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.Shopping_CRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCrudApplication.class, args);
+	}
+
+}
